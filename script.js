b920df28c04477f8c0c94cdbc44bd7ce02cae866
PRATHAP BANDI <126871309+BandiPrathap@users.noreply.github.com>
2024-02-17 12:48:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-let circleBtn=document.getElementById("circleBtn");
-let crossBtn=document.getElementById("crossBtn");
+
 let statusE1=document.getElementById("status");
 let reStart=document.getElementById("restart");
 let statusContainer=document.getElementById("statusContainer");
@@ -19,10 +18,11 @@ let box7=document.getElementById("box7");
 let box8=document.getElementById("box8");
 let box9=document.getElementById("box9");
 
-let twoPlayersBtn="false";
-let circleStatus="false";
-let crossStatus="false";
+let twoPlayersBtn=false;
+let circleStatus=true;
+let crossStatus=false;
 
+reStart.textContent="PLAY";
 	
 let resultArray=[
 	[1,2,3],
@@ -37,20 +37,9 @@ let resultArray=[
 let crossArray=[];
 let circleArray=[];
 
-circleBtn.onclick=function(){
-	
-	circleStatus="true";
-	crossStatus="false";
-};
-
-crossBtn.onclick=function(){
-		
-	circleStatus="false";
-	crossStatus="true";
-};
-
 
 function checkCircleStatus(circleArray,crossArray){
+	let wow=true;
 	for (let i of resultArray){
 		let c=0;
 		for (let j of i){
@@ -61,28 +50,34 @@ function checkCircleStatus(circleArray,crossArray){
 			}
 		}
 		if(c===3){
+			statusContainer.classList.add("d-block");
 			statusE1.textContent="O is Winner!!";
 			statusE1.classList.add("statusE");
-			note.classList.add("d-block");
-			statusContainer.classList.add("d-block");
-			circleBtn.classList.add("d-none");
-			crossBtn.classList.add("d-none");
+			note.classList.add("d-block");			
+			crossArray.splice(0,crossArray.length);
+			circleArray.splice(0,circleArray.length);
+			crossStatus=false;
+			circleStatus=false;
+			reStart.textContent="RESTART";
+			wow=false;
 			break;
 		}
-		if ((crossArray.length + circleArray.length) === 9) {
+		if (((crossArray.length + circleArray.length) === 9)&&wow) {			
 			statusE1.textContent = "DRAW!!";
-			statusE1.classList.add("draw");
+			statusE1.classList.add("draw");			
 			note.classList.add("d-block");
-			statusContainer.classList.add("d-block");
-			circleBtn.classList.add("d-none");
-			crossBtn.classList.add("d-none");
+			crossArray.splice(0,crossArray.length);
+			circleArray.splice(0,circleArray.length);
+			crossStatus=false;
+			circleStatus=false;
+			reStart.textContent="RESTART"
 			break;
 		}
-
 	}
 }
 
 function checkCrossStatus(crossArray,circleArray){
+	let wow=true
 	for (let i of resultArray){
 		let c=0;
 		for (let j of i){
@@ -96,44 +91,52 @@ function checkCrossStatus(crossArray,circleArray){
 			statusContainer.classList.add("d-block");
 			statusE1.textContent="X is Winner!!";
 			statusE1.classList.add("statusE");
-			note.classList.add("d-block");			
-			circleBtn.classList.add("d-none");
-			crossBtn.classList.add("d-none");
+			note.classList.add("d-block");	
+			crossArray.splice(0,crossArray.length);
+			circleArray.splice(0,circleArray.length);
+			crossStatus=false;
+			circleStatus=false;
+			reStart.textContent="RESTART";
+			wow=false;
 			break;
 		}
-		if ((crossArray.length + circleArray.length) === 9) {
-			statusContainer.classList.add("d-block");
+		if (((crossArray.length + circleArray.length) === 9)&&wow) {			
 			statusE1.textContent = "DRAW!!";
 			statusE1.classList.add("draw");			
 			note.classList.add("d-block");
-			circleBtn.classList.add("d-none");
-			crossBtn.classList.add("d-none");
+			crossArray.splice(0,crossArray.length);
+			circleArray.splice(0,circleArray.length);
+			crossStatus=false;
+			circleStatus=false;
+			reStart.textContent="RESTART";
 			break;
 		}
-
 	}
 }
 	
 				
 	box1.onclick=function(){
-		if(circleStatus==="false"&&crossStatus==="false"){
-			alert("select your choice");
+		if(circleStatus===false&&crossStatus===false){
+			alert("RESTART THE GAME...");
 		}
 		if(box1.textContent===""){
-			if(circleStatus==="true"){
+			if(circleStatus===true){
 				box1.textContent="O";
 				box1.style.fontSize="30px";
 				circleArray.push(1);
-				circleStatus="false";
+				circleStatus=false;
+				crossStatus=true;
 				if(circleArray.length>=3){
 					checkCircleStatus(circleArray,crossArray);
+					
 				}
 			}
-			if(crossStatus==="true"){
+			else{
 				box1.textContent="X";
 				box1.style.fontSize="30px";
 				crossArray.push(1);
-				crossStatus="false";
+				crossStatus=false;
+				circleStatus=true;
 				if(crossArray.length>=3){
 					checkCrossStatus(crossArray,circleArray);
 				}
@@ -142,24 +145,26 @@ function checkCrossStatus(crossArray,circleArray){
 	};
 
 	box2.onclick=function(){
-		if(circleStatus==="false"&&crossStatus==="false"){
-			alert("select your choice");
+		if(circleStatus===false&&crossStatus===false){
+			alert("RESTART THE GAME...");
 		}
 		if(box2.textContent===""){
-			if(circleStatus==="true"){
+			if(circleStatus===true){
 				box2.textContent="O";
 				box2.style.fontSize="30px";
 				circleArray.push(2);
-				circleStatus="false";
+				circleStatus=false;
+				crossStatus=true;
 				if(circleArray.length>=3){
 					checkCircleStatus(circleArray,crossArray);
 				}
 			}
-			if(crossStatus==="true"){
+			else{
 				box2.textContent="X";
 				box2.style.fontSize="30px";
 				crossArray.push(2);
-				crossStatus="false";
+				crossStatus=false;
+				circleStatus=true;
 				if(crossArray.length>=3){
 					checkCrossStatus(crossArray,circleArray);
 				}
@@ -168,24 +173,26 @@ function checkCrossStatus(crossArray,circleArray){
 	};
 
 	box3.onclick=function(){
-		if(circleStatus==="false"&&crossStatus==="false"){
-			alert("select your choice");
+		if(circleStatus===false&&crossStatus===false){
+			alert("RESTART THE GAME...");
 		}
 		if(box3.textContent===""){
-			if(circleStatus==="true"){
+			if(circleStatus===true){
 				box3.textContent="O";
 				box3.style.fontSize="30px";
 				circleArray.push(3);
-				circleStatus="false";
+				circleStatus=false;
+				crossStatus=true;
 				if(circleArray.length>=3){
 					checkCircleStatus(circleArray,crossArray);
 				}
 			}
-			if(crossStatus==="true"){
+			else{
 				box3.textContent="X";
 				box3.style.fontSize="30px";
 				crossArray.push(3);
-				crossStatus="false";
+				crossStatus=false;
+				circleStatus=true;
 				if(crossArray.length>=3){
 					checkCrossStatus(crossArray,circleArray);
 				}
@@ -194,24 +201,26 @@ function checkCrossStatus(crossArray,circleArray){
 	};
 
 	box4.onclick=function(){
-		if(circleStatus==="false"&&crossStatus==="false"){
-			alert("select your choice");
+		if(circleStatus===false&&crossStatus===false){
+			alert("RESTART THE GAME...");
 		}
 		if(box4.textContent===""){
-			if(circleStatus==="true"){
+			if(circleStatus===true){
 				box4.textContent="O";
 				box4.style.fontSize="30px";
 				circleArray.push(4);
-				circleStatus="false";
+				circleStatus=false;
+				crossStatus=true;
 				if(circleArray.length>=3){
 					checkCircleStatus(circleArray,crossArray);
 				}
 			}
-			if(crossStatus==="true"){
+			else{
 				box4.textContent="X";
 				box4.style.fontSize="30px";
 				crossArray.push(4);
-				crossStatus="false";
+				crossStatus=false;
+				circleStatus=true;
 				if(crossArray.length>=3){
 					checkCrossStatus(crossArray,circleArray);
 				}
@@ -220,24 +229,26 @@ function checkCrossStatus(crossArray,circleArray){
 	};
 
 	box5.onclick=function(){
-		if(circleStatus==="false"&&crossStatus==="false"){
-			alert("select your choice");
+		if(circleStatus===false&&crossStatus===false){
+			alert("RESTART THE GAME...");
 		}
 		if(box5.textContent===""){
-			if(circleStatus==="true"){
+			if(circleStatus===true){
 				box5.textContent="O";
 				box5.style.fontSize="30px";
 				circleArray.push(5);
-				circleStatus="false";
+				circleStatus=false;
+				crossStatus=true;
 				if(circleArray.length>=3){
 					checkCircleStatus(circleArray,crossArray);
 				}
 			}
-			if(crossStatus==="true"){
+			else{
 				box5.textContent="X";
 				box5.style.fontSize="30px";
 				crossArray.push(5);
-				crossStatus="false";
+				crossStatus=false;
+				circleStatus=true;
 				if(crossArray.length>=3){
 					checkCrossStatus(crossArray,circleArray);
 				}
@@ -246,24 +257,26 @@ function checkCrossStatus(crossArray,circleArray){
 	};
 
 	box6.onclick=function(){
-		if(circleStatus==="false"&&crossStatus==="false"){
-			alert("select your choice");
+		if(circleStatus===false&&crossStatus===false){
+			alert("RESTART THE GAME...");
 		}
 		if(box6.textContent===""){
-			if(circleStatus==="true"){
+			if(circleStatus===true){
 				box6.textContent="O";
 				box6.style.fontSize="30px";
 				circleArray.push(6);
-				circleStatus="false";
+				circleStatus=false;
+				crossStatus=true;
 				if(circleArray.length>=3){
 					checkCircleStatus(circleArray,crossArray);
 				}
 			}
-			if(crossStatus==="true"){
+			else{
 				box6.textContent="X";
 				box6.style.fontSize="30px";
 				crossArray.push(6);
-				crossStatus="false";
+				crossStatus=false;
+				circleStatus=true;
 				if(crossArray.length>=3){
 					checkCrossStatus(crossArray,circleArray);
 				}
@@ -272,24 +285,26 @@ function checkCrossStatus(crossArray,circleArray){
 	};
 
 	box7.onclick=function(){
-		if(circleStatus==="false"&&crossStatus==="false"){
-			alert("select your choice");
+		if(circleStatus===false&&crossStatus===false){
+			alert("RESTART THE GAME...");
 		}
 		if(box7.textContent===""){
-			if(circleStatus==="true"){
+			if(circleStatus===true){
 				box7.textContent="O";
 				box7.style.fontSize="30px";
 				circleArray.push(7);
-				circleStatus="false";
+				circleStatus=false;
+				crossStatus=true;
 				if(circleArray.length>=3){
 					checkCircleStatus(circleArray,crossArray);
 				}
 			}
-			if(crossStatus==="true"){
+			else{
 				box7.textContent="X";
 				box7.style.fontSize="30px";
 				crossArray.push(7);
-				crossStatus="false";
+				crossStatus=false;
+				circleStatus=true;
 				if(crossArray.length>=3){
 					checkCrossStatus(crossArray,circleArray);
 				}
@@ -298,24 +313,26 @@ function checkCrossStatus(crossArray,circleArray){
 	};
 
 	box8.onclick=function(){
-		if(circleStatus==="false"&&crossStatus==="false"){
-			alert("select your choice");
+		if(circleStatus===false&&crossStatus===false){
+			alert("RESTART THE GAME...");
 		}
 		if(box8.textContent===""){
-			if(circleStatus==="true"){
+			if(circleStatus===true){
 				box8.textContent="O";
 				box8.style.fontSize="30px";
 				circleArray.push(8);
-				circleStatus="false";
+				circleStatus=false;
+				crossStatus=true;
 				if(circleArray.length>=3){
 					checkCircleStatus(circleArray,crossArray);
 				}
 			}
-			if(crossStatus==="true"){
+			else{
 				box8.textContent="X";
 				box8.style.fontSize="30px";
 				crossArray.push(8);
-				crossStatus="false";
+				crossStatus=false;
+				circleStatus=true;
 				if(crossArray.length>=3){
 					checkCrossStatus(crossArray,circleArray);
 				}
@@ -324,24 +341,26 @@ function checkCrossStatus(crossArray,circleArray){
 	};
 
 	box9.onclick=function(){
-		if(circleStatus==="false"&&crossStatus==="false"){
-			alert("select your choice");
+		if(circleStatus===false&&crossStatus===false){
+			alert("RESTART THE GAME...");
 		}
 		if(box9.textContent===""){
-			if(circleStatus==="true"){
+			if(circleStatus===true){
 				box9.textContent="O";
 				box9.style.fontSize="30px";
 				circleArray.push(9);
-				circleStatus="false";
+				circleStatus=false;
+				crossStatus=true;
 				if(circleArray.length>=3){
 					checkCircleStatus(circleArray,crossArray);
 				}
 			}
-			if(crossStatus==="true"){
+			else{
 				box9.textContent="X";
 				box9.style.fontSize="30px";
 				crossArray.push(9);
-				crossStatus="false";
+				crossStatus=false;
+				circleStatus=true;
 				if(crossArray.length>=3){
 					checkCrossStatus(crossArray,circleArray);
 				}
@@ -351,15 +370,13 @@ function checkCrossStatus(crossArray,circleArray){
 
 
 reStart.onclick=function(){
-	circleBtn.classList.add("d-block");
-	crossBtn.classList.add("d-block");
 	note.classList.add("d-none");
 	statusE1.textContent="";
 	statusContainer.classList.add("d-none");
 	statusE1.classList.remove("statusE");
 	statusE1.classList.remove("draw");
 	crossArray.splice(0,crossArray.length);
-	circleArray.splice(0,circleArray.length)
+	circleArray.splice(0,circleArray.length);
 	box1.textContent="";
 	box2.textContent="";
 	box3.textContent="";
@@ -369,5 +386,7 @@ reStart.onclick=function(){
 	box7.textContent="";
 	box8.textContent="";
 	box9.textContent="";
-	
+	circleStatus=true;
+	crossStatus=false;
+	reStart.textContent="PLAY";
 }
